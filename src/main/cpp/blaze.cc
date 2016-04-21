Stranciz6/bazel

commit 67a2d585aa43a4a64ff53a85461fa8de6942b375

Update the BlazeServer classes to conform to the Google style guide.
--
MOS_MIGRATED_REVID=120315714

src/main/cpp/blaze.cc
@@ -110,9 +110,6 @@ class BlazeServer {
 };
 
 class AfUnixBlazeServer : public BlazeServer {
- private:
-  int server_socket;
-
  public:
   AfUnixBlazeServer();
   virtual ~AfUnixBlazeServer() {}
@@ -121,7 +118,37 @@ class AfUnixBlazeServer : public BlazeServer {
   void Disconnect() override;
   void Communicate() override;
   void Cancel() override;
+
+ private:
+  int server_socket_;
 };
+
+class GrpcBlazeServer : public BlazeServer {
+ public:
+  GrpcBlazeServer();
+  virtual ~GrpcBlazeServer() {}
+
+  bool Connect() override;
+  void Disconnect() override;
+  void Communicate() override;
+  void Cancel() override;
+
+ private:
+  enum CancelThreadAction { NOTHING, JOIN, CANCEL };
+
+  std::unique_ptr<command_server::CommandServer::Stub> client_;
+  std::string request_cookie_;
+  std::string response_cookie_;
+  std::string command_id_;
+
+  std::condition_variable cancel_thread_signal_;
+  // protects command_id_ and cancel_thread_action_
+  std::mutex cancel_thread_mutex_;
+  CancelThreadAction cancel_thread_action_;
+
+  void CancelThread();
+};
+
 
 ////////////////////////////////////////////////////////////////////////
 // Global Variables
@@ -558,32 +585,6 @@ static int StartServer() {
   return -1;
 }
 
-class GrpcBlazeServer : public BlazeServer {
- private:
-  enum CancelThreadAction { NOTHING, JOIN, CANCEL };
-
-  std::unique_ptr<command_server::CommandServer::Stub> client;
-
-  std::string request_cookie;
-  std::string response_cookie;
-  std::string command_id;
-
-  std::condition_variable cancel_thread_signal;
-  // protects command_id and cancel_thread_action
-  std::mutex cancel_thread_mutex;
-  CancelThreadAction cancel_thread_action;
-  void CancelThread();
-
- public:
-  GrpcBlazeServer();
-  virtual ~GrpcBlazeServer() {}
-
-  bool Connect() override;
-  void Disconnect() override;
-  void Communicate() override;
-  void Cancel() override;
-};
-
 static bool KillRunningServerIfAny(BlazeServer *server);
 
 // Replace this process with blaze in standalone/batch mode.
@@ -634,18 +635,18 @@ static void StartStandalone(BlazeServer* server) {
 }
 
 AfUnixBlazeServer::AfUnixBlazeServer() {
-  server_socket = -1;
+  server_socket_ = -1;
 }
 
 bool AfUnixBlazeServer::Connect() {
-  if (server_socket == -1) {
-    server_socket = socket(PF_UNIX, SOCK_STREAM, 0);
-    if (server_socket == -1)  {
+  if (server_socket_ == -1) {
+    server_socket_ = socket(PF_UNIX, SOCK_STREAM, 0);
+    if (server_socket_ == -1)  {
       pdie(blaze_exit_code::LOCAL_ENVIRONMENTAL_ERROR,
            "can't create AF_UNIX socket");
     }
 
-    if (fcntl(server_socket, F_SETFD, FD_CLOEXEC) == -1) {
+    if (fcntl(server_socket_, F_SETFD, FD_CLOEXEC) == -1) {
       pdie(blaze_exit_code::LOCAL_ENVIRONMENTAL_ERROR,
            "fcntl(F_SETFD, FD_CLOEXEC) failed");
     }
@@ -661,7 +662,7 @@ bool AfUnixBlazeServer::Connect() {
     addr.sun_path[sizeof addr.sun_path - 1] = '\0';
     free(resolved_path);
     sockaddr *paddr = reinterpret_cast<sockaddr *>(&addr);
-    int result = connect(server_socket, paddr, sizeof addr);
+    int result = connect(server_socket_, paddr, sizeof addr);
     return result == 0;
   } else if (errno == ENOENT) {  // No socket means no server to connect to
     errno = ECONNREFUSED;
@@ -674,11 +675,11 @@ bool AfUnixBlazeServer::Connect() {
 }
 
 void AfUnixBlazeServer::Disconnect() {
-  close(server_socket);
-  server_socket = -1;
+  close(server_socket_);
+  server_socket_ = -1;
 }
 
-static ATTRIBUTE_NORETURN void server_eof() {
+static ATTRIBUTE_NORETURN void ServerEof() {
   // e.g. external SIGKILL of server, misplaced System.exit() in the server,
   // or a JVM crash. Print out the jvm.out file in case there's something
   // useful.
@@ -690,15 +691,15 @@ static ATTRIBUTE_NORETURN void server_eof() {
 }
 
 // Reads a single char from the specified stream.
-static unsigned char read_server_char(int fd) {
+static unsigned char ReadServerChar(int fd) {
   unsigned char result;
   if (read(fd, &result, 1) != 1) {
-    server_eof();
+    ServerEof();
   }
   return result;
 }
 
-static unsigned int read_server_int(int fd) {
+static unsigned int ReadServerInt(int fd) {
   unsigned char buffer[4];
   unsigned char *p = buffer;
   int remaining = 4;
@@ -706,7 +707,7 @@ static unsigned int read_server_int(int fd) {
   while (remaining > 0) {
     int bytes_read = read(fd, p, remaining);
     if (bytes_read <= 0) {
-      server_eof();
+      ServerEof();
     }
 
     remaining -= bytes_read;
@@ -719,12 +720,12 @@ static unsigned int read_server_int(int fd) {
 static char server_output_buffer[8192];
 
 static void forward_server_output(int socket, int output) {
-  unsigned int remaining = read_server_int(socket);
+  unsigned int remaining = ReadServerInt(socket);
   while (remaining > 0) {
     int bytes = remaining > 8192 ? 8192 : remaining;
     bytes = read(socket, server_output_buffer, bytes);
     if (bytes <= 0) {
-      server_eof();
+      ServerEof();
     }
 
     remaining -= bytes;
@@ -742,11 +743,11 @@ void AfUnixBlazeServer::Communicate() {
   request_size[1] = (request.size() >> 16) & 0xff;
   request_size[2] = (request.size() >> 8) & 0xff;
   request_size[3] = (request.size()) & 0xff;
-  if (write(server_socket, request_size, 4) != 4) {
+  if (write(server_socket_, request_size, 4) != 4) {
     pdie(blaze_exit_code::INTERNAL_ERROR, "write() to server failed");
   }
 
-  if (write(server_socket, request.data(), request.size()) != request.size()) {
+  if (write(server_socket_, request.data(), request.size()) != request.size()) {
     pdie(blaze_exit_code::INTERNAL_ERROR, "write() to server failed");
   }
 
@@ -757,11 +758,11 @@ void AfUnixBlazeServer::Communicate() {
   while (true) {
     fd_set fdset;
     FD_ZERO(&fdset);
-    FD_SET(server_socket, &fdset);
+    FD_SET(server_socket_, &fdset);
     struct timeval timeout;
     timeout.tv_sec = 3;
     timeout.tv_usec = 0;
-    int result = select(server_socket + 1, &fdset, NULL, &fdset, &timeout);
+    int result = select(server_socket_ + 1, &fdset, NULL, &fdset, &timeout);
     if (result > 0) {
       // Data is ready on socket.  Go ahead and read it.
       break;
@@ -787,16 +788,16 @@ void AfUnixBlazeServer::Communicate() {
   const int TAG_CONTROL = 3;
   for (;;) {
     // Read the tag
-    char tag = read_server_char(server_socket);
+    char tag = ReadServerChar(server_socket_);
     switch (tag) {
       // stdout
       case TAG_STDOUT:
-        forward_server_output(server_socket, 1);
+        forward_server_output(server_socket_, 1);
         break;
 
       // stderr
       case TAG_STDERR:
-        forward_server_output(server_socket, 2);
+        forward_server_output(server_socket_, 2);
         break;
 
       // Control stream. Currently only used for reporting the exit code.
@@ -808,18 +809,18 @@ void AfUnixBlazeServer::Communicate() {
           raise(globals->received_signal);
           exit(1);  // (in case raise didn't kill us for some reason)
         } else {
-          unsigned int length = read_server_int(server_socket);
+          unsigned int length = ReadServerInt(server_socket_);
           if (length != 4) {
-            server_eof();
+            ServerEof();
           }
-          unsigned int exit_code = read_server_int(server_socket);
+          unsigned int exit_code = ReadServerInt(server_socket_);
           exit(exit_code);
         }
         break;  // Control never gets here, only for code beauty
 
       default:
         fprintf(stderr, "bad tag %d\n", tag);
-        server_eof();
+        ServerEof();
         break;  // Control never gets here, only for code beauty
     }
   }
@@ -1844,11 +1845,11 @@ bool GrpcBlazeServer::Connect() {
     return false;
   }
 
-  if (!ReadFile(server_dir + "/request_cookie", &request_cookie)) {
+  if (!ReadFile(server_dir + "/request_cookie_", &request_cookie_)) {
     return false;
   }
 
-  if (!ReadFile(server_dir + "/response_cookie", &response_cookie)) {
+  if (!ReadFile(server_dir + "/response_cookie_", &response_cookie_)) {
     return false;
   }
 
@@ -1863,14 +1864,14 @@ bool GrpcBlazeServer::Connect() {
 
   command_server::PingRequest request;
   command_server::PingResponse response;
-  request.set_cookie(request_cookie);
-  grpc::Status status = client->Ping(&context, request, &response);
+  request.set_cookie(request_cookie_);
+  grpc::Status status = client_->Ping(&context, request, &response);
 
   if (!status.ok()) {
     return false;
   }
 
-  this->client = std::move(client);
+  this->client_ = std::move(client);
   return true;
 }
 
@@ -1897,29 +1898,29 @@ bool GrpcBlazeServer::Connect() {
 // to the server)
 void GrpcBlazeServer::CancelThread() {
   bool running = true;
-  std::unique_lock<std::mutex> lock(cancel_thread_mutex);
+  std::unique_lock<std::mutex> lock(cancel_thread_mutex_);
   while (running) {
-    cancel_thread_signal.wait(lock);
-    switch (cancel_thread_action) {
+    cancel_thread_signal_.wait(lock);
+    switch (cancel_thread_action_) {
       case JOIN:
         running = false;
-        cancel_thread_action = NOTHING;
+        cancel_thread_action_ = NOTHING;
         break;
 
       case CANCEL:
         // If we don't know the command ID yet, we'll be woken up when it
         // becomes known.
-        if (command_id.size() > 0) {
+        if (command_id_.size() > 0) {
           command_server::CancelRequest request;
-          request.set_cookie(request_cookie);
-          request.set_command_id(command_id);
+          request.set_cookie(request_cookie_);
+          request.set_command_id(command_id_);
           grpc::ClientContext context;
           context.set_deadline(std::chrono::system_clock::now() +
                                std::chrono::milliseconds(100));
           command_server::CancelResponse response;
           // There isn't a lot we can do if this request fails
-          client->Cancel(&context, request, &response);
-          cancel_thread_action = NOTHING;
+          client_->Cancel(&context, request, &response);
+          cancel_thread_action_ = NOTHING;
         }
         break;
 
@@ -1940,7 +1941,7 @@ void GrpcBlazeServer::Communicate() {
   globals->option_processor.GetCommandArguments(&arg_vector);
 
   command_server::RunRequest request;
-  request.set_cookie(request_cookie);
+  request.set_cookie(request_cookie_);
   request.set_block_for_lock(globals->options.block_for_lock);
   request.set_client_description("pid=" + ToString(getpid()));
   for (const string& arg : arg_vector) {
@@ -1950,9 +1951,9 @@ void GrpcBlazeServer::Communicate() {
   grpc::ClientContext context;
   command_server::RunResponse response;
   std::unique_ptr<grpc::ClientReader<command_server::RunResponse>> reader(
-      client->Run(&context, request));
+      client_->Run(&context, request));
 
-  cancel_thread_action = NOTHING;
+  cancel_thread_action_ = NOTHING;
   std::thread cancel_thread(&GrpcBlazeServer::CancelThread, this);
   bool command_id_set = false;
   while (reader->Read(&response)) {
@@ -1967,19 +1968,19 @@ void GrpcBlazeServer::Communicate() {
     }
 
     if (!command_id_set && response.command_id().size() > 0) {
-      std::unique_lock<std::mutex> lock(cancel_thread_mutex);
-      command_id = response.command_id();
+      std::unique_lock<std::mutex> lock(cancel_thread_mutex_);
+      command_id_ = response.command_id();
       command_id_set = true;
       // Wake up the cancellation thread in case there is a pending cancellation
-      cancel_thread_signal.notify_one();
+      cancel_thread_signal_.notify_one();
     }
   }
 
   {
     // Wake up the cancellation thread so that it can finish
-    std::unique_lock<std::mutex> lock(cancel_thread_mutex);
-    cancel_thread_action = JOIN;
-    cancel_thread_signal.notify_one();
+    std::unique_lock<std::mutex> lock(cancel_thread_mutex_);
+    cancel_thread_action_ = JOIN;
+    cancel_thread_signal_.notify_one();
   }
   cancel_thread.join();
 
@@ -1992,16 +1993,16 @@ void GrpcBlazeServer::Communicate() {
 }
 
 void GrpcBlazeServer::Disconnect() {
-  client.reset();
-  request_cookie = "";
-  response_cookie = "";
+  client_.reset();
+  request_cookie_ = "";
+  response_cookie_ = "";
 }
 
 void GrpcBlazeServer::Cancel() {
-  std::unique_lock<std::mutex> lock(cancel_thread_mutex);
+  std::unique_lock<std::mutex> lock(cancel_thread_mutex_);
   // Wake up the cancellation thread and tell it to issue its RPC
-  cancel_thread_action = CANCEL;
-  cancel_thread_signal.notify_one();
+  cancel_thread_action_ = CANCEL;
+  cancel_thread_signal_.notify_one();
 }
 
 }  // namespace blaze

commit a4431e3f44031ba3e80d3113aac55735d73f6084

Assorted changes wrt. gRPC client/server comms:
- Actually make it work again (commit 67a2d585aa43a4a64ff53a85461fa8de6942b375 broke it, maybe I shouldn't send out changes in a hurry next time)
- Rename --grpc_port to --command_port (it's a bit better name)
- Do not send a kill signal to the server that can't be connected if we only connect to it to verify its presence

--
MOS_MIGRATED_REVID=120418784

src/main/cpp/blaze.cc
@@ -363,8 +363,9 @@ static vector<string> GetArgumentArray() {
     result.push_back("--batch");
   }
 
-  if (globals->options.grpc_port != -1) {
-    result.push_back("--grpc_port=" + ToString(globals->options.grpc_port));
+  if (globals->options.command_port != -1) {
+    result.push_back(
+        "--command_port=" + ToString(globals->options.command_port));
   }
 
   result.push_back("--install_base=" +
@@ -890,7 +891,9 @@ static bool ConnectToServer(BlazeServer *server, bool start) {
            "can't get server pid from connection");
     }
     return true;
-  } else {
+  }
+
+  if (start) {
     // If we couldn't connect to the server check if there is still a PID file
     // and if so, kill the server that wrote it. This can happen e.g. if the
     // server is in a GC pause and therefore cannot respond to ping requests and
@@ -900,9 +903,7 @@ static bool ConnectToServer(BlazeServer *server, bool start) {
     if (server_pid >= 0) {
       kill(server_pid, SIGKILL);
     }
-  }
 
-  if (start) {
     SetScheduling(globals->options.batch_cpu_scheduling,
                   globals->options.io_nice_level);
 
@@ -1803,11 +1804,11 @@ int main(int argc, const char *argv[]) {
   const string self_path = GetSelfPath();
   ComputeBaseDirectories(self_path);
 
-  blaze_server = globals->options.grpc_port >= 0
+  blaze_server = globals->options.command_port >= 0
       ? static_cast<BlazeServer *>(new GrpcBlazeServer())
       : static_cast<BlazeServer *>(new AfUnixBlazeServer());
 
-  if (globals->options.grpc_port < 0 || globals->options.batch) {
+  if (globals->options.command_port < 0 || globals->options.batch) {
     // The gRPC server can handle concurrent commands just fine. However, we
     // need to be careful not to start two parallel instances in batch mode.
     AcquireLock();
@@ -1841,15 +1842,15 @@ bool GrpcBlazeServer::Connect() {
   std::string server_dir = globals->options.output_base + "/server";
   std::string port;
 
-  if (!ReadFile(server_dir + "/grpc_port", &port)) {
+  if (!ReadFile(server_dir + "/command_port", &port)) {
     return false;
   }
 
-  if (!ReadFile(server_dir + "/request_cookie_", &request_cookie_)) {
+  if (!ReadFile(server_dir + "/request_cookie", &request_cookie_)) {
     return false;
   }
 
-  if (!ReadFile(server_dir + "/response_cookie_", &response_cookie_)) {
+  if (!ReadFile(server_dir + "/response_cookie", &response_cookie_)) {
     return false;
   }
 
@@ -1865,7 +1866,7 @@ bool GrpcBlazeServer::Connect() {
   command_server::PingRequest request;
   command_server::PingResponse response;
   request.set_cookie(request_cookie_);
-  grpc::Status status = client_->Ping(&context, request, &response);
+  grpc::Status status = client->Ping(&context, request, &response);
 
   if (!status.ok()) {
     return false;

src/main/cpp/blaze_startup_options.cc
@@ -56,7 +56,7 @@ BlazeStartupOptions::BlazeStartupOptions(const BlazeStartupOptions &rhs)
       watchfs(rhs.watchfs),
       allow_configurable_attributes(rhs.allow_configurable_attributes),
       option_sources(rhs.option_sources),
-      grpc_port(rhs.grpc_port),
+      command_port(rhs.command_port),
       invocation_policy(rhs.invocation_policy),
       host_javabase(rhs.host_javabase) {}
 

src/main/cpp/blaze_startup_options.h
@@ -212,7 +212,7 @@ class BlazeStartupOptions {
 
   // Port for gRPC command server. 0 means let the kernel choose, -1 means no
   // gRPC command server.
-  int grpc_port;
+  int command_port;
 
   // Invocation policy proto. May be NULL.
   const char* invocation_policy;

src/main/cpp/blaze_startup_options_common.cc
@@ -50,7 +50,7 @@ void BlazeStartupOptions::Init() {
   // 3 hours (but only 5 seconds if used within a test)
   max_idle_secs = testing ? 5 : (3 * 3600);
   oom_more_eagerly_threshold = 100;
-  grpc_port = -1;
+  command_port = -1;
   oom_more_eagerly = false;
   watchfs = false;
   invocation_policy = NULL;
@@ -81,7 +81,7 @@ void BlazeStartupOptions::Copy(
   lhs->batch_cpu_scheduling = rhs.batch_cpu_scheduling;
   lhs->io_nice_level = rhs.io_nice_level;
   lhs->max_idle_secs = rhs.max_idle_secs;
-  lhs->grpc_port = rhs.grpc_port;
+  lhs->command_port = rhs.command_port;
   lhs->oom_more_eagerly = rhs.oom_more_eagerly;
   lhs->watchfs = rhs.watchfs;
   lhs->allow_configurable_attributes = rhs.allow_configurable_attributes;
@@ -233,11 +233,11 @@ blaze_exit_code::ExitCode BlazeStartupOptions::ProcessArg(
     watchfs = false;
     option_sources["watchfs"] = rcfile;
   } else if ((value = GetUnaryOption(
-      arg, next_arg, "--grpc_port")) != NULL) {
-    if (!blaze_util::safe_strto32(value, &grpc_port) ||
-        grpc_port < -1 || grpc_port > 65535) {
+      arg, next_arg, "--command_port")) != NULL) {
+    if (!blaze_util::safe_strto32(value, &command_port) ||
+        command_port < -1 || command_port > 65535) {
       blaze_util::StringPrintf(error,
-          "Invalid argument to --grpc_port: '%s'. "
+          "Invalid argument to --command_port: '%s'. "
           "Must be a valid port number or -1 to disable the gRPC server.\n",
           value);
       return blaze_exit_code::BAD_ARGV;

src/main/java/com/google/devtools/build/lib/runtime/BlazeRuntime.java
@@ -877,7 +877,7 @@ public final class BlazeRuntime {
     CommandExecutor commandExecutor = new CommandExecutor(runtime, dispatcher);
 
 
-    if (startupOptions.grpcPort != -1) {
+    if (startupOptions.commandPort != -1) {
       try {
         // This is necessary so that Bazel kind of works during bootstrapping, at which time the
         // gRPC server is not compiled in so that we don't need gRPC for bootstrapping.
@@ -885,7 +885,7 @@ public final class BlazeRuntime {
             "com.google.devtools.build.lib.server.GrpcServerImpl$Factory");
         RPCServer.Factory factory = (RPCServer.Factory) factoryClass.newInstance();
         return factory.create(commandExecutor, runtime.getClock(),
-            startupOptions.grpcPort, runtime.getServerDirectory());
+            startupOptions.commandPort, runtime.getServerDirectory());
       } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
         throw new AbruptExitException("gRPC server not compiled in", ExitCode.BLAZE_INTERNAL_ERROR);
       }

src/main/java/com/google/devtools/build/lib/runtime/BlazeServerStartupOptions.java
@@ -258,9 +258,10 @@ public class BlazeServerStartupOptions extends OptionsBase {
           + "specify --invocation_policy multiple times.")
   public String invocationPolicy;
 
-  @Option(name = "grpc_port",
+  @Option(name = "command_port",
       defaultValue = "-1",
       category = "undocumented",
-      help = "Port to start up the gRPC command server on. If 0, let the kernel choose.")
-  public int grpcPort;
+      help = "Port to start up the gRPC command server on. If 0, let the kernel choose. If -1, "
+          + "use a custom protocol on an AF_UNIX socket.")
+  public int commandPort;
 }

src/main/java/com/google/devtools/build/lib/server/GrpcServerImpl.java
@@ -125,7 +125,7 @@ public class GrpcServerImpl extends RPCServer implements CommandServerGrpc.Comma
   }
 
   // These paths are all relative to the server directory
-  private static final String PORT_FILE = "grpc_port";
+  private static final String PORT_FILE = "command_port";
   private static final String REQUEST_COOKIE_FILE = "request_cookie";
   private static final String RESPONSE_COOKIE_FILE = "response_cookie";
 