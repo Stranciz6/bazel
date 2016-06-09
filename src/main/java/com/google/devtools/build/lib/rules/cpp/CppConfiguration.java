Stranciz6/bazel

commit bfc628a29d68d022cbea2c551ceece825298cdb3

Move -MD and -MF(for dotd file generation) into feature
--
MOS_MIGRATED_REVID=124355936

src/main/java/com/google/devtools/build/lib/rules/cpp/CppConfiguration.java
@@ -692,6 +692,38 @@ public class CppConfiguration extends BuildConfiguration.Fragment {
       return toolchain;
     }
     try {
+      if (!features.contains("dependency_file")) {
+        // Gcc options:
+        //  -MD turns on .d file output as a side-effect (doesn't imply -E)
+        //  -MM[D] enables user includes only, not system includes
+        //  -MF <name> specifies the dotd file name
+        // Issues:
+        //  -M[M] alone subverts actual .o output (implies -E)
+        //  -M[M]D alone breaks some of the .d naming assumptions
+        // This combination gets user and system includes with specified name:
+        //  -MD -MF <name>
+        TextFormat.merge(""
+            + "feature {"
+            + "  name: 'dependency_file'"
+            + "  flag_set {"
+            + "    action: 'assemble'"
+            + "    action: 'preprocess-assemble'"
+            + "    action: 'c-compile'"
+            + "    action: 'c++-compile'"
+            + "    action: 'c++-module-compile'"
+            + "    action: 'objc-compile'"
+            + "    action: 'objc++-compile'"
+            + "    expand_if_all_available: 'dependency_file'"
+            + "    flag_group {"
+            + "      flag: '-MD'"
+            + "      flag: '-MF'"
+            + "      flag: '%{dependency_file}'"
+            + "    }"
+            + "  }"
+            + "}",
+            toolchainBuilder);
+      }
+
       if (!features.contains("random_seed")) {
         // GCC and Clang give randomized names to symbols which are defined in
         // an anonymous namespace but have external linkage.  To make

src/main/java/com/google/devtools/build/lib/rules/cpp/CcCommon.java
@@ -83,6 +83,7 @@ public final class CcCommon {
    * Features we request to enable unless a rule explicitly doesn't support them.
    */
   private static final ImmutableSet<String> DEFAULT_FEATURES = ImmutableSet.of(
+      CppRuleClasses.DEPENDENCY_FILE,
       CppRuleClasses.COMPILE_ACTION_FLAGS_IN_FLAG_SET,
       CppRuleClasses.RANDOM_SEED,
       CppRuleClasses.MODULE_MAPS,

src/main/java/com/google/devtools/build/lib/rules/cpp/CppCompileAction.java
@@ -1340,22 +1340,6 @@ public class CppCompileAction extends AbstractAction
         }
       }
 
-      // Enable <object>.d file generation.
-      if (dotdFile != null) {
-        // Gcc options:
-        //  -MD turns on .d file output as a side-effect (doesn't imply -E)
-        //  -MM[D] enables user includes only, not system includes
-        //  -MF <name> specifies the dotd file name
-        // Issues:
-        //  -M[M] alone subverts actual .o output (implies -E)
-        //  -M[M]D alone breaks some of the .d naming assumptions
-        // This combination gets user and system includes with specified name:
-        //  -MD -MF <name>
-        options.add("-MD");
-        options.add("-MF");
-        options.add(dotdFile.getSafeExecPath().getPathString());
-      }
-
       if (!featureConfiguration.isEnabled("compile_action_flags_in_flag_set")) {
         if (FileType.contains(outputFile, CppFileTypes.ASSEMBLER, CppFileTypes.PIC_ASSEMBLER)) {
           options.add("-S");

src/main/java/com/google/devtools/build/lib/rules/cpp/CppCompileActionBuilder.java
@@ -426,6 +426,10 @@ public class CppCompileActionBuilder {
     return this;
   }
 
+  public DotdFile getDotdFile() {
+    return this.dotdFile;
+  }
+
   public CppCompileActionBuilder setGcnoFile(Artifact gcnoFile) {
     this.gcnoFile = gcnoFile;
     return this;

src/main/java/com/google/devtools/build/lib/rules/cpp/CppModel.java
@@ -29,6 +29,7 @@ import com.google.devtools.build.lib.packages.BuildType;
 import com.google.devtools.build.lib.rules.cpp.CcCompilationOutputs.Builder;
 import com.google.devtools.build.lib.rules.cpp.CcToolchainFeatures.FeatureConfiguration;
 import com.google.devtools.build.lib.rules.cpp.CcToolchainFeatures.Variables.VariablesExtension;
+import com.google.devtools.build.lib.rules.cpp.CppCompileAction.DotdFile;
 import com.google.devtools.build.lib.rules.cpp.Link.LinkStaticness;
 import com.google.devtools.build.lib.rules.cpp.Link.LinkTargetType;
 import com.google.devtools.build.lib.rules.cpp.LinkerInputs.LibraryToLink;
@@ -369,10 +370,11 @@ public final class CppModel {
 
     CppCompilationContext builderContext = builder.getContext();
     CppModuleMap cppModuleMap = builderContext.getCppModuleMap();
+    Artifact sourceFile = builder.getSourceFile();
     Artifact outputFile = builder.getOutputFile();
     String realOutputFilePath;
 
-    buildVariables.addVariable("source_file", builder.getSourceFile().getExecPathString());
+    buildVariables.addVariable("source_file", sourceFile.getExecPathString());
     buildVariables.addVariable("output_file", outputFile.getExecPathString());
 
     if (fake) {
@@ -389,6 +391,13 @@ public final class CppModel {
       buildVariables.addVariable("output_preprocess_file", realOutputFilePath);
     } else {
       buildVariables.addVariable("output_object_file", realOutputFilePath);
+    }
+
+    DotdFile dotdFile = CppFileTypes.mustProduceDotdFile(sourceFile.getPath().toString())
+        ? Preconditions.checkNotNull(builder.getDotdFile()) : null;
+    // Set dependency_file to enable <object>.d file generation.
+    if (dotdFile != null) {
+      buildVariables.addVariable("dependency_file", dotdFile.getSafeExecPath().getPathString());
     }
 
     if (featureConfiguration.isEnabled(CppRuleClasses.MODULE_MAPS) && cppModuleMap != null) {

src/main/java/com/google/devtools/build/lib/rules/cpp/CppRuleClasses.java
@@ -109,6 +109,11 @@ public class CppRuleClasses {
   public static final String COMPILE_ACTION_FLAGS_IN_FLAG_SET = "compile_action_flags_in_flag_set";
 
   /**
+   * A string constant for the dependency_file feature. This feature generates the .d file.
+   */
+  public static final String DEPENDENCY_FILE = "dependency_file";
+
+  /**
    * A string constant for the module_map_home_cwd feature.
    */
   public static final String MODULE_MAP_HOME_CWD = "module_map_home_cwd";

src/main/java/com/google/devtools/build/lib/rules/objc/ExperimentalObjcLibrary.java
@@ -169,6 +169,7 @@ public class ExperimentalObjcLibrary implements RuleConfiguredTargetFactory {
     // We create a module map by default to allow for swift interop.
     activatedCrosstoolSelectables.add(CppRuleClasses.MODULE_MAPS);
     activatedCrosstoolSelectables.add(CppRuleClasses.COMPILE_ACTION_FLAGS_IN_FLAG_SET);
+    activatedCrosstoolSelectables.add(CppRuleClasses.DEPENDENCY_FILE);
 
     return toolchain.getFeatures().getFeatureConfiguration(activatedCrosstoolSelectables.build());
   }

commit a9c916ef4da454fc5306e96aa6726338aa11a52a

Add missing actions to dependency_file feature
This fixes the breakage caused by commit bfc628a29d68d022cbea2c551ceece825298cdb3
Also modified test to check options for generating dependency file.

--
MOS_MIGRATED_REVID=124441471

src/main/java/com/google/devtools/build/lib/rules/cpp/CppConfiguration.java
@@ -713,6 +713,8 @@ public class CppConfiguration extends BuildConfiguration.Fragment {
             + "    action: 'c++-module-compile'"
             + "    action: 'objc-compile'"
             + "    action: 'objc++-compile'"
+            + "    action: 'c++-header-preprocessing'"
+            + "    action: 'c++-header-parsing'"
             + "    expand_if_all_available: 'dependency_file'"
             + "    flag_group {"
             + "      flag: '-MD'"