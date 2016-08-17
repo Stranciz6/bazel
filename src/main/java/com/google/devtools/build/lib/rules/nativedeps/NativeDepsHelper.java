Stranciz6/bazel

commit 48aaceaab3f16b0b1f1bea8e6592f2c1ba34d9d3

Record the category of the artifact to be linked in LinkerInput.
It's currently only used for sanity checks, but the idea is that we'll use this field to decide what to do with a given linker input instead of inferring things from its file name.

Also make artifact name creation a bit simpler by using the same set of variables for compiler and linker outputs.

--
MOS_MIGRATED_REVID=129990944

src/main/java/com/google/devtools/build/lib/rules/nativedeps/NativeDepsHelper.java
@@ -21,6 +21,7 @@ import com.google.devtools.build.lib.analysis.RuleContext;
 import com.google.devtools.build.lib.analysis.actions.SymlinkAction;
 import com.google.devtools.build.lib.analysis.config.BuildConfiguration;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
+import com.google.devtools.build.lib.rules.cpp.ArtifactCategory;
 import com.google.devtools.build.lib.rules.cpp.CcLinkParams;
 import com.google.devtools.build.lib.rules.cpp.CcToolchainProvider;
 import com.google.devtools.build.lib.rules.cpp.CppBuildInfo;
@@ -160,10 +161,10 @@ public abstract class NativeDepsHelper {
     CppLinkActionBuilder builder =
         new CppLinkActionBuilder(ruleContext, sharedLibrary, configuration, toolchain);
     if (useDynamicRuntime) {
-      builder.setRuntimeInputs(
+      builder.setRuntimeInputs(ArtifactCategory.DYNAMIC_LIBRARY,
           toolchain.getDynamicRuntimeLinkMiddleman(), toolchain.getDynamicRuntimeLinkInputs());
     } else {
-      builder.setRuntimeInputs(
+      builder.setRuntimeInputs(ArtifactCategory.DYNAMIC_LIBRARY,
           toolchain.getStaticRuntimeLinkMiddleman(), toolchain.getStaticRuntimeLinkInputs());
     }
     CppLinkAction linkAction =

src/main/java/com/google/devtools/build/lib/rules/cpp/ArtifactCategory.java
@@ -19,9 +19,7 @@ package com.google.devtools.build.lib.rules.cpp;
  */
 public enum ArtifactCategory {
   STATIC_LIBRARY("lib%{base_name}.a"),
-  PIC_STATIC_LIBRARY("lib%{base_name}.pic.a"),
   ALWAYSLINK_STATIC_LIBRARY("lib%{base_name}.lo"),
-  ALWAYSLINK_PIC_STATIC_LIBRARY("lib%{base_name}.pic.lo"),
   DYNAMIC_LIBRARY("lib%{base_name}.so"),
   EXECUTABLE("%{base_name}"),
   INTERFACE_LIBRARY("lib%{base_name}.ifso"),
@@ -38,12 +36,8 @@ public enum ArtifactCategory {
 
   private final String defaultPattern;
 
-  private ArtifactCategory(String defaultPattern) {
+  ArtifactCategory(String defaultPattern) {
     this.defaultPattern = defaultPattern;
-  }
-
-  private ArtifactCategory() {
-    this.defaultPattern = null;
   }
 
   /** Returns the name of the category. */

src/main/java/com/google/devtools/build/lib/rules/cpp/CcBinary.java
@@ -190,7 +190,13 @@ public abstract class CcBinary implements RuleConfiguredTargetFactory {
     if (!isLinkShared(ruleContext)) {
       binaryPath = new PathFragment(binaryPath.getPathString() + OsUtils.executableExtension());
     }
+
     Artifact binary = ruleContext.getBinArtifact(binaryPath);
+    if (isLinkShared(ruleContext) && !CppFileTypes.SHARED_LIBRARY.matches(binary.getFilename())) {
+      ruleContext.attributeError("linkshared", "'linkshared' used in non-shared library");
+      return null;
+    }
+
     CppLinkActionBuilder linkActionBuilder =
         determineLinkerArguments(
             ruleContext,
@@ -208,10 +214,12 @@ public abstract class CcBinary implements RuleConfiguredTargetFactory {
     CcToolchainProvider ccToolchain = CppHelper.getToolchain(ruleContext);
     if (linkStaticness == LinkStaticness.DYNAMIC) {
       linkActionBuilder.setRuntimeInputs(
+          ArtifactCategory.DYNAMIC_LIBRARY,
           ccToolchain.getDynamicRuntimeLinkMiddleman(),
           ccToolchain.getDynamicRuntimeLinkInputs());
     } else {
       linkActionBuilder.setRuntimeInputs(
+          ArtifactCategory.STATIC_LIBRARY,
           ccToolchain.getStaticRuntimeLinkMiddleman(),
           ccToolchain.getStaticRuntimeLinkInputs());
       // Only force a static link of libgcc if static runtime linking is enabled (which
@@ -258,15 +266,11 @@ public abstract class CcBinary implements RuleConfiguredTargetFactory {
     LibraryToLink outputLibrary = linkAction.getOutputLibrary();
     Iterable<Artifact> fakeLinkerInputs =
         fake ? linkAction.getInputs() : ImmutableList.<Artifact>of();
-    Artifact executable = outputLibrary.getArtifact();
+    Artifact executable = linkAction.getLinkOutput();
     CcLinkingOutputs.Builder linkingOutputsBuilder = new CcLinkingOutputs.Builder();
     if (isLinkShared(ruleContext)) {
-      if (CppFileTypes.SHARED_LIBRARY.matches(binary.getFilename())) {
-        linkingOutputsBuilder.addDynamicLibrary(outputLibrary);
-        linkingOutputsBuilder.addExecutionDynamicLibrary(outputLibrary);
-      } else {
-        ruleContext.attributeError("linkshared", "'linkshared' used in non-shared library");
-      }
+      linkingOutputsBuilder.addDynamicLibrary(outputLibrary);
+      linkingOutputsBuilder.addExecutionDynamicLibrary(outputLibrary);
     }
     // Also add all shared libraries from srcs.
     for (Artifact library : precompiledFiles.getSharedLibraries()) {
@@ -403,7 +407,7 @@ public abstract class CcBinary implements RuleConfiguredTargetFactory {
     Iterable<Artifact> objectFiles = compilationOutputs.getObjectFiles(usePic);
 
     if (fake) {
-      builder.addFakeNonLibraryInputs(objectFiles);
+      builder.addFakeObjectFiles(objectFiles);
     } else {
       builder.addObjectFiles(objectFiles);
     }
@@ -421,7 +425,8 @@ public abstract class CcBinary implements RuleConfiguredTargetFactory {
             common.getDynamicLibrarySymlink(library, true), library,
             CcLinkingOutputs.libraryIdentifierOf(library)));
       } else {
-        builder.addLibrary(LinkerInputs.precompiledLibraryToLink(library));
+        builder.addLibrary(LinkerInputs.precompiledLibraryToLink(
+            library, ArtifactCategory.STATIC_LIBRARY));
       }
     }
 

src/main/java/com/google/devtools/build/lib/rules/cpp/CcLibrary.java
@@ -218,8 +218,8 @@ public abstract class CcLibrary implements RuleConfiguredTargetFactory {
      *
      * Note that some target platforms do not require shared library code to be PIC.
      */
-    Iterable<LibraryToLink> staticLibrariesFromSrcs =
-        LinkerInputs.opaqueLibrariesToLink(precompiledFiles.getStaticLibraries());
+    Iterable<LibraryToLink> staticLibrariesFromSrcs = LinkerInputs.opaqueLibrariesToLink(
+        ArtifactCategory.STATIC_LIBRARY, precompiledFiles.getStaticLibraries());
     helper.addStaticLibraries(staticLibrariesFromSrcs);
     helper.addPicStaticLibraries(Iterables.filter(staticLibrariesFromSrcs, PIC_STATIC_FILTER));
     helper.addPicStaticLibraries(precompiledFiles.getPicStaticLibraries());

src/main/java/com/google/devtools/build/lib/rules/cpp/CcLibraryHelper.java
@@ -44,6 +44,7 @@ import com.google.devtools.build.lib.rules.cpp.CcToolchainFeatures.FeatureConfig
 import com.google.devtools.build.lib.rules.cpp.CcToolchainFeatures.Variables.VariablesExtension;
 import com.google.devtools.build.lib.rules.cpp.CppConfiguration.HeadersCheckingMode;
 import com.google.devtools.build.lib.rules.cpp.Link.LinkTargetType;
+import com.google.devtools.build.lib.rules.cpp.Link.Staticness;
 import com.google.devtools.build.lib.rules.cpp.LinkerInputs.LibraryToLink;
 import com.google.devtools.build.lib.syntax.Type;
 import com.google.devtools.build.lib.util.FileType;
@@ -924,7 +925,7 @@ public final class CcLibraryHelper {
       //
       // An additional pre-existing issue is that the header check tokens are dropped if we don't
       // generate any link actions, effectively disabling header checking in some cases.
-      if (linkType.isStaticLibraryLink()) {
+      if (linkType.staticness() == Staticness.STATIC) {
         // TODO(bazel-team): This can't create the link action for a cc_binary yet.
         ccLinkingOutputs = model.createCcLinkActions(ccOutputs, nonCodeLinkerInputs);
       }

src/main/java/com/google/devtools/build/lib/rules/cpp/CcToolchainFeatures.java
@@ -26,7 +26,6 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
-import com.google.devtools.build.lib.analysis.RuleContext;
 import com.google.devtools.build.lib.analysis.config.InvalidConfigurationException;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.Immutable;
 import com.google.devtools.build.lib.vfs.PathFragment;
@@ -732,21 +731,20 @@ public class CcToolchainFeatures implements Serializable {
     }
 
     /**
-     * Returns the artifact name that pattern selects for a given rule.
+     * Returns the artifact name that this pattern selects.
      */
-    public String getArtifactName(RuleContext ruleContext, Map<String, String> extraVariables) {
-      StringBuilder result = new StringBuilder();
-      PathFragment nameFragment = new PathFragment(ruleContext.getLabel().getName());
+    public String getArtifactName(Map<String, String> variables) {
+      StringBuilder resultBuilder = new StringBuilder();
       Variables.View artifactNameVariables =
           new Variables.Builder()
-              .addVariable("base_name", nameFragment.getBaseName())
-              .addAllVariables(extraVariables)
+              .addAllVariables(variables)
               .build()
-              .getView(variables);
+              .getView(this.variables);
       for (StringChunk chunk : chunks) {
-        chunk.expand(artifactNameVariables.getVariables(), result);
+        chunk.expand(artifactNameVariables.getVariables(), resultBuilder);
       }
-      return result.toString();
+      String result = resultBuilder.toString();
+      return result.charAt(0) == '/' ? result.substring(1) : result;
     }
   }
   
@@ -1453,8 +1451,10 @@ public class CcToolchainFeatures implements Serializable {
    * Returns the artifact selected by the toolchain for the given action type and action category,
    * or null if the category is not supported by the action config.
    */
-  String getArtifactNameForCategory(ArtifactCategory artifactCategory, RuleContext ruleContext,
-      Map<String, String> extraVariables) throws ExpansionException {
+  String getArtifactNameForCategory(ArtifactCategory artifactCategory, String outputName)
+      throws ExpansionException {
+    PathFragment output = new PathFragment(outputName);
+
     ArtifactNamePattern patternForCategory = null;
     for (ArtifactNamePattern artifactNamePattern : artifactNamePatterns) {
       if (artifactNamePattern.getArtifactCategory() == artifactCategory) {
@@ -1467,7 +1467,10 @@ public class CcToolchainFeatures implements Serializable {
               MISSING_ARTIFACT_NAME_PATTERN_ERROR_TEMPLATE, artifactCategory.getCategoryName()));
     }
 
-    return patternForCategory.getArtifactName(ruleContext, extraVariables);
+    return patternForCategory.getArtifactName(ImmutableMap.of(
+        "output_name", outputName,
+        "base_name", output.getBaseName(),
+        "output_directory", output.getParentDirectory().getPathString()));
   }
 
   /** Returns true if the toolchain defines an ArtifactNamePattern for the given category. */

src/main/java/com/google/devtools/build/lib/rules/cpp/CppCompileActionBuilder.java
@@ -381,7 +381,7 @@ public class CppCompileActionBuilder {
   public CppCompileActionBuilder setOutputs(
       ArtifactCategory outputCategory, String outputName, boolean generateDotd) {
     this.outputFile = CppHelper.getCompileOutputArtifact(
-        ruleContext, CppHelper.getCompileArtifactName(ruleContext, outputCategory, outputName));
+        ruleContext, CppHelper.getArtifactNameForCategory(ruleContext, outputCategory, outputName));
     if (generateDotd) {
       String dotdFileName = CppHelper.getDotdFileName(ruleContext, outputCategory, outputName);
       if (configuration.getFragment(CppConfiguration.class).getInmemoryDotdFiles()) {

src/main/java/com/google/devtools/build/lib/rules/cpp/CppHelper.java
@@ -16,7 +16,6 @@ package com.google.devtools.build.lib.rules.cpp;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 import com.google.devtools.build.lib.actions.ActionOwner;
 import com.google.devtools.build.lib.actions.Artifact;
@@ -610,10 +609,9 @@ public class CppHelper {
         ruleContext.getConfiguration().getBinDirectory(ruleContext.getRule().getRepository()));
   }
 
-  static String getCompileArtifactName(RuleContext ruleContext, ArtifactCategory category,
-      String base) {
-    return getToolchain(ruleContext).getFeatures().getArtifactNameForCategory(
-        category, ruleContext, ImmutableMap.of("output_name", base));
+  static String getArtifactNameForCategory(RuleContext ruleContext, ArtifactCategory category,
+      String outputName) {
+    return getToolchain(ruleContext).getFeatures().getArtifactNameForCategory(category, outputName);
   }
 
   static String getDotdFileName(RuleContext ruleContext, ArtifactCategory outputCategory,
@@ -621,8 +619,8 @@ public class CppHelper {
     String baseName = outputCategory == ArtifactCategory.OBJECT_FILE
         || outputCategory == ArtifactCategory.PROCESSED_HEADER
         ? outputName
-        : getCompileArtifactName(ruleContext, outputCategory, outputName);
+        : getArtifactNameForCategory(ruleContext, outputCategory, outputName);
 
-    return getCompileArtifactName(ruleContext, ArtifactCategory.INCLUDED_FILE_LIST, baseName);
+    return getArtifactNameForCategory(ruleContext, ArtifactCategory.INCLUDED_FILE_LIST, baseName);
   }
 }

commit 2304b44c6a2f3c384fdf0c54b28c72344bd93c89

Make native deps with embedded runtimes work again. commit 48aaceaab3f16b0b1f1bea8e6592f2c1ba34d9d3 broke it.
--
MOS_MIGRATED_REVID=130496094

src/main/java/com/google/devtools/build/lib/rules/nativedeps/NativeDepsHelper.java
@@ -168,7 +168,7 @@ public abstract class NativeDepsHelper {
       builder.setRuntimeInputs(ArtifactCategory.DYNAMIC_LIBRARY,
           toolchain.getDynamicRuntimeLinkMiddleman(), toolchain.getDynamicRuntimeLinkInputs());
     } else {
-      builder.setRuntimeInputs(ArtifactCategory.DYNAMIC_LIBRARY,
+      builder.setRuntimeInputs(ArtifactCategory.STATIC_LIBRARY,
           toolchain.getStaticRuntimeLinkMiddleman(), toolchain.getStaticRuntimeLinkInputs());
     }
     CppLinkAction linkAction =