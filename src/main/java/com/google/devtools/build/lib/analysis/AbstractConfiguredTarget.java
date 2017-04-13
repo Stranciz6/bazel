Stranciz6/bazel

commit 7849de33a6b3bee140ce68c13c968204c9bdea06

Implement default provider
Default providers can now be used not only to return standard providers values
from a rule implementation function, but also to access these values provided
by other rules.
PiperOrigin-RevId: 152797193

src/main/java/com/google/devtools/build/lib/analysis/AbstractConfiguredTarget.java
@@ -16,7 +16,6 @@ package com.google.devtools.build.lib.analysis;
 
 import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableList;
-import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.analysis.config.BuildConfiguration;
 import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
@@ -27,10 +26,10 @@ import com.google.devtools.build.lib.packages.ClassObjectConstructor;
 import com.google.devtools.build.lib.packages.PackageSpecification;
 import com.google.devtools.build.lib.packages.SkylarkProviderIdentifier;
 import com.google.devtools.build.lib.packages.Target;
+import com.google.devtools.build.lib.rules.SkylarkRuleContext;
 import com.google.devtools.build.lib.syntax.ClassObject;
 import com.google.devtools.build.lib.syntax.EvalException;
 import com.google.devtools.build.lib.syntax.EvalUtils;
-import com.google.devtools.build.lib.syntax.SkylarkNestedSet;
 import javax.annotation.Nullable;
 
 /**
@@ -108,19 +107,17 @@ public abstract class AbstractConfiguredTarget
 
   @Override
   public Object getValue(String name) {
+    // Standard fields should be proxied to their default provider object
+    DefaultProvider defaultProvider =
+        (DefaultProvider) get(SkylarkRuleContext.getDefaultProvider().getKey());
     switch (name) {
+      case FILES_FIELD:
+      case DEFAULT_RUNFILES_FIELD:
+      case DATA_RUNFILES_FIELD:
+      case FilesToRunProvider.SKYLARK_NAME:
+        return defaultProvider.getValue(name);
       case LABEL_FIELD:
         return getLabel();
-      case FILES_FIELD:
-        // A shortcut for files to build in Skylark. FileConfiguredTarget and RuleConfiguredTarget
-        // always has FileProvider and Error- and PackageGroupConfiguredTarget-s shouldn't be
-        // accessible in Skylark.
-        return SkylarkNestedSet.of(
-            Artifact.class, getProvider(FileProvider.class).getFilesToBuild());
-      case DEFAULT_RUNFILES_FIELD:
-        return RunfilesProvider.DEFAULT_RUNFILES.apply(this);
-      case DATA_RUNFILES_FIELD:
-        return RunfilesProvider.DATA_RUNFILES.apply(this);
       default:
         return get(name);
     }
@@ -173,6 +170,10 @@ public abstract class AbstractConfiguredTarget
   @Override
   public ImmutableCollection<String> getKeys() {
     return ImmutableList.of(
-        DATA_RUNFILES_FIELD, DEFAULT_RUNFILES_FIELD, LABEL_FIELD, FILES_FIELD);
+        DATA_RUNFILES_FIELD,
+        DEFAULT_RUNFILES_FIELD,
+        LABEL_FIELD,
+        FILES_FIELD,
+        FilesToRunProvider.SKYLARK_NAME);
   }
 }

src/main/java/com/google/devtools/build/lib/analysis/DefaultProvider.java
@@ -1,0 +1,58 @@
+// Copyright 2017 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.devtools.build.lib.analysis;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.devtools.build.lib.actions.Artifact;
+import com.google.devtools.build.lib.concurrent.ThreadSafety.Immutable;
+import com.google.devtools.build.lib.packages.ClassObjectConstructor;
+import com.google.devtools.build.lib.packages.SkylarkClassObject;
+import com.google.devtools.build.lib.rules.SkylarkRuleContext;
+import com.google.devtools.build.lib.syntax.SkylarkNestedSet;
+import java.util.Map;
+
+/** DefaultProvider is provided by all targets implicitly and contains all standard fields. */
+@Immutable
+public final class DefaultProvider extends SkylarkClassObject {
+
+  // Accessors for Skylark
+  private static final String DATA_RUNFILES_FIELD = "data_runfiles";
+  private static final String DEFAULT_RUNFILES_FIELD = "default_runfiles";
+  private static final String FILES_FIELD = "files";
+
+  private DefaultProvider(ClassObjectConstructor constructor, Map<String, Object> values) {
+    super(constructor, values);
+  }
+
+  public static DefaultProvider build(
+      RunfilesProvider runfilesProvider,
+      FileProvider fileProvider,
+      FilesToRunProvider filesToRunProvider) {
+    ImmutableMap.Builder<String, Object> attrBuilder = new ImmutableMap.Builder<>();
+    if (runfilesProvider != null) {
+      attrBuilder.put(DATA_RUNFILES_FIELD, runfilesProvider.getDataRunfiles());
+      attrBuilder.put(DEFAULT_RUNFILES_FIELD, runfilesProvider.getDefaultRunfiles());
+    } else {
+      attrBuilder.put(DATA_RUNFILES_FIELD, Runfiles.EMPTY);
+      attrBuilder.put(DEFAULT_RUNFILES_FIELD, Runfiles.EMPTY);
+    }
+
+    attrBuilder.put(
+        FILES_FIELD, SkylarkNestedSet.of(Artifact.class, fileProvider.getFilesToBuild()));
+    attrBuilder.put(FilesToRunProvider.SKYLARK_NAME, filesToRunProvider);
+
+    ClassObjectConstructor constructor = SkylarkRuleContext.getDefaultProvider();
+    return new DefaultProvider(constructor, attrBuilder.build());
+  }
+}

src/main/java/com/google/devtools/build/lib/analysis/FileConfiguredTarget.java
@@ -14,6 +14,7 @@
 
 package com.google.devtools.build.lib.analysis;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
 import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
@@ -21,6 +22,7 @@ import com.google.devtools.build.lib.collect.nestedset.Order;
 import com.google.devtools.build.lib.packages.ClassObjectConstructor;
 import com.google.devtools.build.lib.packages.FileTarget;
 import com.google.devtools.build.lib.packages.SkylarkClassObject;
+import com.google.devtools.build.lib.rules.SkylarkRuleContext;
 import com.google.devtools.build.lib.rules.fileset.FilesetProvider;
 import com.google.devtools.build.lib.rules.test.InstrumentedFilesProvider;
 import com.google.devtools.build.lib.util.FileType;
@@ -40,12 +42,22 @@ public abstract class FileConfiguredTarget extends AbstractConfiguredTarget
     super(targetContext);
     NestedSet<Artifact> filesToBuild = NestedSetBuilder.create(Order.STABLE_ORDER, artifact);
     this.artifact = artifact;
+    FileProvider fileProvider = new FileProvider(filesToBuild);
+    FilesToRunProvider filesToRunProvider =
+        FilesToRunProvider.fromSingleExecutableArtifact(artifact);
+    SkylarkClassObject defaultProvider =
+        DefaultProvider.build(null, fileProvider, filesToRunProvider);
+    SkylarkProviders skylarkProviders =
+        new SkylarkProviders(
+            ImmutableMap.<String, Object>of(),
+            ImmutableMap.of(SkylarkRuleContext.getDefaultProvider().getKey(), defaultProvider));
     TransitiveInfoProviderMap.Builder builder =
         TransitiveInfoProviderMap.builder()
             .put(VisibilityProvider.class, this)
             .put(LicensesProvider.class, this)
-            .add(new FileProvider(filesToBuild))
-            .add(FilesToRunProvider.fromSingleExecutableArtifact(artifact));
+            .put(SkylarkProviders.class, skylarkProviders)
+            .add(fileProvider)
+            .add(filesToRunProvider);
     if (this instanceof FilesetProvider) {
       builder.put(FilesetProvider.class, this);
     }
@@ -86,6 +98,6 @@ public abstract class FileConfiguredTarget extends AbstractConfiguredTarget
   @Nullable
   @Override
   public SkylarkClassObject get(ClassObjectConstructor.Key providerKey) {
-    return null;
+    return getProvider(SkylarkProviders.class).getDeclaredProvider(providerKey);
   }
 }

src/main/java/com/google/devtools/build/lib/analysis/RuleConfiguredTargetBuilder.java
@@ -92,7 +92,6 @@ public final class RuleConfiguredTargetBuilder {
         getFilesToRun(runfilesSupport, filesToBuild), runfilesSupport, executable);
     addProvider(new FileProvider(filesToBuild));
     addProvider(filesToRunProvider);
-    addSkylarkTransitiveInfo(FilesToRunProvider.SKYLARK_NAME, filesToRunProvider);
 
     if (runfilesSupport != null) {
       // If a binary is built, build its runfiles, too
@@ -135,6 +134,14 @@ public final class RuleConfiguredTargetBuilder {
       addProvider(OutputGroupProvider.class, outputGroupProvider);
       addSkylarkTransitiveInfo(OutputGroupProvider.SKYLARK_NAME, outputGroupProvider);
     }
+
+    // Populate default provider fields and build it
+    DefaultProvider defaultProvider =
+        DefaultProvider.build(
+            providersBuilder.getProvider(RunfilesProvider.class),
+            providersBuilder.getProvider(FileProvider.class),
+            filesToRunProvider);
+    skylarkDeclaredProviders.put(defaultProvider.getConstructor().getKey(), defaultProvider);
 
     TransitiveInfoProviderMap providers = providersBuilder.build();
     addRegisteredProvidersToSkylarkProviders(providers);

src/main/java/com/google/devtools/build/lib/analysis/OutputGroupProvider.java
@@ -53,7 +53,7 @@ import javax.annotation.Nullable;
 @Immutable
 public final class OutputGroupProvider implements
     TransitiveInfoProvider, SkylarkIndexable, Iterable<String> {
-  public static String SKYLARK_NAME = "output_groups";
+  public static final String SKYLARK_NAME = "output_groups";
 
   /**
    * Prefix for output groups that are not reported to the user on the terminal output of Blaze when

src/main/java/com/google/devtools/build/lib/rules/SkylarkRuleConfiguredTargetBuilder.java
@@ -13,7 +13,6 @@
 // limitations under the License.
 package com.google.devtools.build.lib.rules;
 
-import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.devtools.build.lib.actions.Artifact;
@@ -270,7 +269,7 @@ public final class SkylarkRuleConfiguredTargetBuilder {
     }
 
     if (!isParsed) {
-      addSimpleProviders(builder, ruleContext, loc, executable, null, null, null, null);
+      addSimpleProviders(builder, ruleContext, loc, executable, null, null, null);
     }
 
     try {
@@ -365,18 +364,19 @@ public final class SkylarkRuleConfiguredTargetBuilder {
       }
     }
 
-    addSimpleProviders(builder, ruleContext, loc, executable, statelessRunfiles, dataRunfiles,
-        defaultRunfiles, (isDefaultProvider ? provider : null));
+    addSimpleProviders(
+        builder, ruleContext, loc, executable, statelessRunfiles, dataRunfiles, defaultRunfiles);
   }
 
-  private static void addSimpleProviders(RuleConfiguredTargetBuilder builder,
+  private static void addSimpleProviders(
+      RuleConfiguredTargetBuilder builder,
       RuleContext ruleContext,
       Location loc,
       Artifact executable,
       Runfiles statelessRunfiles,
       Runfiles dataRunfiles,
-      Runfiles defaultRunfiles,
-      SkylarkClassObject defaultProvider) throws EvalException {
+      Runfiles defaultRunfiles)
+      throws EvalException {
 
     if ((statelessRunfiles != null) && (dataRunfiles != null || defaultRunfiles != null)) {
       throw new EvalException(loc, "Cannot specify the provider 'runfiles' "
@@ -414,19 +414,6 @@ public final class SkylarkRuleConfiguredTargetBuilder {
           ruleContext.getAnalysisEnvironment().getRegisteredActions());
       builder.addSkylarkDeclaredProvider(actions, loc);
     }
-
-    // Populate default provider fields and build it
-    ImmutableMap.Builder<String, Object> attrBuilder = new ImmutableMap.Builder<>();
-    // TODO: Add actual attributes that users expect to access from default providers
-    attrBuilder.put("runfiles", runfilesProvider);
-    SkylarkClassObject statelessDefaultProvider =
-        new SkylarkClassObject(
-            SkylarkRuleContext.getDefaultProvider(),
-            attrBuilder.build());
-
-    // Add the default provider
-    builder.addSkylarkDeclaredProvider(statelessDefaultProvider, (defaultProvider == null) ? loc
-        : Optional.fromNullable(defaultProvider.getCreationLocOrNull()).or(loc));
   }
 
   private static <T> T cast(String paramName, ClassObject struct, Class<T> expectedGenericType,

src/main/java/com/google/devtools/build/lib/rules/SkylarkRuleContext.java
@@ -594,13 +594,19 @@ public final class SkylarkRuleContext implements SkylarkValue {
         }
       };
 
-  @SkylarkCallable(name = "default_provider", structField = true,
-      doc = "A provider that's provided by every rule, even if it's not returned explicitly. "
-          + "A <code>default_provider</code> accepts all special parameters that can be returned "
-          + "from rule implementation function in a struct, which are <code>runfiles</code>, "
-          + "<code>data_runfiles</code>, <code>default_runfiles</code>, "
-          + "<code>output_groups</code>, <code>instrumented_files</code>, and all "
-          + "<a href=\"skylark-provider.html\">providers</a> that are available on built-in rules.")
+  @SkylarkCallable(
+    name = "default_provider",
+    structField = true,
+    doc = "A provider that's provided by every rule, even if it's not returned explicitly. "
+        + "A <code>default_provider</code> accepts all special parameters that can be returned "
+        + "from rule implementation function in a struct, which are <code>runfiles</code>, "
+        + "<code>data_runfiles</code>, <code>default_runfiles</code>, "
+        + "<code>output_groups</code>, <code>instrumented_files</code>, and all "
+        + "<a href=\"skylark-provider.html\">providers</a> that are available on built-in rules. "
+        + "Each instance of the default provider contains the following standard fields: "
+        + "<code>data_runfiles</code>, <code>default_runfiles</code>, <code>files</code>, "
+        + "and <code>files_to_run</code>. The values of these fields are equivalent to the "
+        + "values of the corresponding fields of the target the default provider belongs to.")
   public static ClassObjectConstructor getDefaultProvider() {
     return DEFAULT_PROVIDER;
   }

commit 8267ad514203e3a384fdbd1f9e1905c638440d9a

Fix memory regression introduced by default providers
The memory regression was introduced in https://github.com/bazelbuild/bazel/commit/7849de33a6b3bee140ce68c13c968204c9bdea06 , now default providers
are optimized and are built only on demand for all types of targets.

PiperOrigin-RevId: 152957220

src/main/java/com/google/devtools/build/lib/analysis/AbstractConfiguredTarget.java
@@ -24,11 +24,13 @@ import com.google.devtools.build.lib.collect.nestedset.Order;
 import com.google.devtools.build.lib.events.Location;
 import com.google.devtools.build.lib.packages.ClassObjectConstructor;
 import com.google.devtools.build.lib.packages.PackageSpecification;
+import com.google.devtools.build.lib.packages.SkylarkClassObject;
 import com.google.devtools.build.lib.packages.SkylarkProviderIdentifier;
 import com.google.devtools.build.lib.packages.Target;
 import com.google.devtools.build.lib.syntax.ClassObject;
 import com.google.devtools.build.lib.syntax.EvalException;
 import com.google.devtools.build.lib.syntax.EvalUtils;
+import java.util.concurrent.atomic.AtomicReference;
 import javax.annotation.Nullable;
 
 /**
@@ -41,6 +43,9 @@ public abstract class AbstractConfiguredTarget
   private final BuildConfiguration configuration;
 
   private final NestedSet<PackageSpecification> visibility;
+
+  // Cached on-demand default provider
+  private final AtomicReference<DefaultProvider> defaultProvider = new AtomicReference<>();
 
   // Accessors for Skylark
   private static final String DATA_RUNFILES_FIELD = "data_runfiles";
@@ -106,15 +111,13 @@ public abstract class AbstractConfiguredTarget
 
   @Override
   public Object getValue(String name) {
-    // Standard fields should be proxied to their default provider object
-    DefaultProvider defaultProvider =
-        (DefaultProvider) get(DefaultProvider.SKYLARK_CONSTRUCTOR.getKey());
     switch (name) {
       case FILES_FIELD:
       case DEFAULT_RUNFILES_FIELD:
       case DATA_RUNFILES_FIELD:
       case FilesToRunProvider.SKYLARK_NAME:
-        return defaultProvider.getValue(name);
+        // Standard fields should be proxied to their default provider object
+        return getDefaultProvider().getValue(name);
       case LABEL_FIELD:
         return getLabel();
       default:
@@ -130,14 +133,10 @@ public abstract class AbstractConfiguredTarget
           EvalUtils.getDataTypeName(key)));
     }
     ClassObjectConstructor constructor = (ClassObjectConstructor) key;
-    SkylarkProviders provider = getProvider(SkylarkProviders.class);
-    if (provider != null) {
-      Object declaredProvider = provider.getDeclaredProvider(constructor.getKey());
-      if (declaredProvider != null) {
-        return declaredProvider;
-      }
+    Object declaredProvider = get(constructor.getKey());
+    if (declaredProvider != null) {
+      return declaredProvider;
     }
-    // Either provider or declaredProvider is null
     throw new EvalException(loc, String.format(
         "Object of type Target doesn't contain declared provider %s",
         constructor.getPrintableName()));
@@ -174,5 +173,31 @@ public abstract class AbstractConfiguredTarget
         LABEL_FIELD,
         FILES_FIELD,
         FilesToRunProvider.SKYLARK_NAME);
+  }
+
+  private DefaultProvider getDefaultProvider() {
+    if (defaultProvider.get() == null) {
+      defaultProvider.compareAndSet(
+          null,
+          DefaultProvider.build(
+              getProvider(RunfilesProvider.class),
+              getProvider(FileProvider.class),
+              getProvider(FilesToRunProvider.class)));
+    }
+    return defaultProvider.get();
+  }
+
+  /** Returns a declared provider provided by this target. Only meant to use from Skylark. */
+  @Nullable
+  @Override
+  public SkylarkClassObject get(ClassObjectConstructor.Key providerKey) {
+    if (providerKey.equals(DefaultProvider.SKYLARK_CONSTRUCTOR.getKey())) {
+      return getDefaultProvider();
+    }
+    SkylarkProviders skylarkProviders = getProvider(SkylarkProviders.class);
+    if (skylarkProviders != null) {
+      return skylarkProviders.getDeclaredProvider(providerKey);
+    }
+    return null;
   }
 }

src/main/java/com/google/devtools/build/lib/analysis/DefaultProvider.java
@@ -13,6 +13,8 @@
 // limitations under the License.
 package com.google.devtools.build.lib.analysis;
 
+import com.google.common.collect.ImmutableCollection;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.Immutable;
@@ -22,6 +24,7 @@ import com.google.devtools.build.lib.packages.NativeClassObjectConstructor;
 import com.google.devtools.build.lib.packages.SkylarkClassObject;
 import com.google.devtools.build.lib.syntax.SkylarkNestedSet;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 /** DefaultProvider is provided by all targets implicitly and contains all standard fields. */
 @Immutable
@@ -31,6 +34,17 @@ public final class DefaultProvider extends SkylarkClassObject {
   private static final String DATA_RUNFILES_FIELD = "data_runfiles";
   private static final String DEFAULT_RUNFILES_FIELD = "default_runfiles";
   private static final String FILES_FIELD = "files";
+  private static final ImmutableList<String> KEYS =
+      ImmutableList.of(
+          DATA_RUNFILES_FIELD,
+          DEFAULT_RUNFILES_FIELD,
+          FILES_FIELD,
+          FilesToRunProvider.SKYLARK_NAME);
+
+  private final RunfilesProvider runfilesProvider;
+  private final FileProvider fileProvider;
+  private final FilesToRunProvider filesToRunProvider;
+  private final AtomicReference<SkylarkNestedSet> files = new AtomicReference<>();
 
   public static final String SKYLARK_NAME = "DefaultInfo";
   public static final ClassObjectConstructor SKYLARK_CONSTRUCTOR =
@@ -43,27 +57,47 @@ public final class DefaultProvider extends SkylarkClassObject {
         }
       };
 
-  private DefaultProvider(ClassObjectConstructor constructor, Map<String, Object> values) {
-    super(constructor, values);
+  private DefaultProvider(
+      ClassObjectConstructor constructor,
+      RunfilesProvider runfilesProvider,
+      FileProvider fileProvider,
+      FilesToRunProvider filesToRunProvider) {
+    // Fields map is not used here to prevent memory regression
+    super(constructor, ImmutableMap.<String, Object>of());
+    this.runfilesProvider = runfilesProvider;
+    this.fileProvider = fileProvider;
+    this.filesToRunProvider = filesToRunProvider;
   }
 
   public static DefaultProvider build(
       RunfilesProvider runfilesProvider,
       FileProvider fileProvider,
       FilesToRunProvider filesToRunProvider) {
-    ImmutableMap.Builder<String, Object> attrBuilder = new ImmutableMap.Builder<>();
-    if (runfilesProvider != null) {
-      attrBuilder.put(DATA_RUNFILES_FIELD, runfilesProvider.getDataRunfiles());
-      attrBuilder.put(DEFAULT_RUNFILES_FIELD, runfilesProvider.getDefaultRunfiles());
-    } else {
-      attrBuilder.put(DATA_RUNFILES_FIELD, Runfiles.EMPTY);
-      attrBuilder.put(DEFAULT_RUNFILES_FIELD, Runfiles.EMPTY);
+    return new DefaultProvider(
+        SKYLARK_CONSTRUCTOR, runfilesProvider, fileProvider, filesToRunProvider);
+  }
+
+  @Override
+  public Object getValue(String name) {
+    switch (name) {
+      case DATA_RUNFILES_FIELD:
+        return (runfilesProvider == null) ? Runfiles.EMPTY : runfilesProvider.getDataRunfiles();
+      case DEFAULT_RUNFILES_FIELD:
+        return (runfilesProvider == null) ? Runfiles.EMPTY : runfilesProvider.getDefaultRunfiles();
+      case FILES_FIELD:
+        if (files.get() == null) {
+          files.compareAndSet(
+              null, SkylarkNestedSet.of(Artifact.class, fileProvider.getFilesToBuild()));
+        }
+        return files.get();
+      case FilesToRunProvider.SKYLARK_NAME:
+        return filesToRunProvider;
     }
+    return null;
+  }
 
-    attrBuilder.put(
-        FILES_FIELD, SkylarkNestedSet.of(Artifact.class, fileProvider.getFilesToBuild()));
-    attrBuilder.put(FilesToRunProvider.SKYLARK_NAME, filesToRunProvider);
-
-    return new DefaultProvider(SKYLARK_CONSTRUCTOR, attrBuilder.build());
+  @Override
+  public ImmutableCollection<String> getKeys() {
+    return KEYS;
   }
 }

src/main/java/com/google/devtools/build/lib/analysis/FileConfiguredTarget.java
@@ -14,18 +14,14 @@
 
 package com.google.devtools.build.lib.analysis;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.collect.nestedset.NestedSet;
 import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
 import com.google.devtools.build.lib.collect.nestedset.Order;
-import com.google.devtools.build.lib.packages.ClassObjectConstructor;
 import com.google.devtools.build.lib.packages.FileTarget;
-import com.google.devtools.build.lib.packages.SkylarkClassObject;
 import com.google.devtools.build.lib.rules.fileset.FilesetProvider;
 import com.google.devtools.build.lib.rules.test.InstrumentedFilesProvider;
 import com.google.devtools.build.lib.util.FileType;
-import javax.annotation.Nullable;
 
 /**
  * A ConfiguredTarget for a source FileTarget.  (Generated files use a
@@ -44,17 +40,10 @@ public abstract class FileConfiguredTarget extends AbstractConfiguredTarget
     FileProvider fileProvider = new FileProvider(filesToBuild);
     FilesToRunProvider filesToRunProvider =
         FilesToRunProvider.fromSingleExecutableArtifact(artifact);
-    SkylarkClassObject defaultProvider =
-        DefaultProvider.build(null, fileProvider, filesToRunProvider);
-    SkylarkProviders skylarkProviders =
-        new SkylarkProviders(
-            ImmutableMap.<String, Object>of(),
-            ImmutableMap.of(DefaultProvider.SKYLARK_CONSTRUCTOR.getKey(), defaultProvider));
     TransitiveInfoProviderMap.Builder builder =
         TransitiveInfoProviderMap.builder()
             .put(VisibilityProvider.class, this)
             .put(LicensesProvider.class, this)
-            .put(SkylarkProviders.class, skylarkProviders)
             .add(fileProvider)
             .add(filesToRunProvider);
     if (this instanceof FilesetProvider) {
@@ -92,11 +81,5 @@ public abstract class FileConfiguredTarget extends AbstractConfiguredTarget
   @Override
   public Object get(String providerKey) {
     return null;
-  }
-
-  @Nullable
-  @Override
-  public SkylarkClassObject get(ClassObjectConstructor.Key providerKey) {
-    return getProvider(SkylarkProviders.class).getDeclaredProvider(providerKey);
   }
 }

src/main/java/com/google/devtools/build/lib/analysis/MergedConfiguredTarget.java
@@ -17,12 +17,9 @@ import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
-import com.google.devtools.build.lib.packages.ClassObjectConstructor;
-import com.google.devtools.build.lib.packages.SkylarkClassObject;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import javax.annotation.Nullable;
 
 /**
  * A single dependency with its configured target and aspects merged together.
@@ -55,12 +52,6 @@ public final class MergedConfiguredTarget extends AbstractConfiguredTarget {
   @Override
   public Object get(String providerKey) {
     return getProvider(SkylarkProviders.class).getValue(providerKey);
-  }
-
-  @Nullable
-  @Override
-  public SkylarkClassObject get(ClassObjectConstructor.Key providerKey) {
-    return getProvider(SkylarkProviders.class).getDeclaredProvider(providerKey);
   }
 
   @Override

src/main/java/com/google/devtools/build/lib/analysis/RuleConfiguredTarget.java
@@ -19,10 +19,8 @@ import com.google.common.collect.ImmutableMap;
 import com.google.devtools.build.lib.analysis.config.ConfigMatchingProvider;
 import com.google.devtools.build.lib.analysis.config.RunUnder;
 import com.google.devtools.build.lib.cmdline.Label;
-import com.google.devtools.build.lib.packages.ClassObjectConstructor;
 import com.google.devtools.build.lib.packages.OutputFile;
 import com.google.devtools.build.lib.packages.Rule;
-import com.google.devtools.build.lib.packages.SkylarkClassObject;
 import com.google.devtools.build.lib.util.Preconditions;
 import javax.annotation.Nullable;
 
@@ -109,15 +107,6 @@ public final class RuleConfiguredTarget extends AbstractConfiguredTarget {
   public Object get(String providerKey) {
     return getProvider(SkylarkProviders.class).getValue(providerKey);
   }
-
-  /**
-   * Returns a declared provider provided by this target. Only meant to use from Skylark.
-   */
-  @Override
-  public SkylarkClassObject get(ClassObjectConstructor.Key providerKey) {
-    return getProvider(SkylarkProviders.class).getDeclaredProvider(providerKey);
-  }
-
 
   @Override
   public final Rule getTarget() {

src/main/java/com/google/devtools/build/lib/analysis/RuleConfiguredTargetBuilder.java
@@ -135,14 +135,6 @@ public final class RuleConfiguredTargetBuilder {
       addSkylarkTransitiveInfo(OutputGroupProvider.SKYLARK_NAME, outputGroupProvider);
     }
 
-    // Populate default provider fields and build it
-    DefaultProvider defaultProvider =
-        DefaultProvider.build(
-            providersBuilder.getProvider(RunfilesProvider.class),
-            providersBuilder.getProvider(FileProvider.class),
-            filesToRunProvider);
-    skylarkDeclaredProviders.put(defaultProvider.getConstructor().getKey(), defaultProvider);
-
     TransitiveInfoProviderMap providers = providersBuilder.build();
     addRegisteredProvidersToSkylarkProviders(providers);
 